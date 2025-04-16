98d2f60f3e502165dcb4222b6cac8d0661fbdcfb
Vasily Melenchuk <bzzzil@gmail.com>
2025-04-16 07:18:33 +0300

    first commit

---
+++ bubble.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90, 42, 99};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    // Display original array
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    // Basic bubble sort implementation
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    
+    // Display sorted array
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
