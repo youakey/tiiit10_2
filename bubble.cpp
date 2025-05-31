680215a705ef2d0cf42190c502ff4ff58395c3e3
chel <myemail.com>
2025-05-31 04:18:54 +0300

    Исправленный bubble.cpp

--- bubble.cpp
+++ bubble.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+using namespace std; // Добавлено для использования cout и endl
 
 int main() {
     int arr[] = {64, 34, 34, 25, 61, 12, 22, 11, 90, 42, 99};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
     
     cout << "Original: ";
     for (int i = 0; i < n; i++) {
-        cout << arr[i << " ";
+        cout << arr[i] << " "; // Исправлено с arr[i << " " на arr[i] << " "
     }
     cout << endl;
     
@@ -20,7 +21,7 @@ int main() {
         }
     }
     
-    cout < "Output: ";
+    cout << "Output: "; // Исправлено с cout < на cout <<
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
